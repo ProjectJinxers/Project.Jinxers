f3bc1c351792321e163c5ce1edf0d766fa3e743e
ProjectJinxers <85575204+ProjectJinxers@users.noreply.github.com>
2021-07-17 13:14:49 +0200

    ˜ support image size attributes, + app icon

--- src/main/java/org/projectjinxers/ui/ProjectJinxers.java
+++ src/main/java/org/projectjinxers/ui/ProjectJinxers.java
@@ -13,10 +13,15 @@
  */
 package org.projectjinxers.ui;
 
+import java.awt.Taskbar;
+import java.awt.Toolkit;
+import java.net.URL;
+
 import org.projectjinxers.ui.main.MainPresenter;
 import org.projectjinxers.ui.main.MainView;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -28,9 +33,32 @@ public class ProjectJinxers extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("ProjectJinxers");
+        Image appIcon = new Image(getClass().getClassLoader().getResourceAsStream("icon.jpeg"));
+        primaryStage.getIcons().add(appIcon);
+        setTaskbarImage();
         MainPresenter mainPresenter = MainView.createMainPresenter(this);
         primaryStage.setScene(mainPresenter.getScene());
         primaryStage.show();
+    }
+
+    private void setTaskbarImage() {
+        final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        final URL imageResource = getClass().getClassLoader().getResource("icon.jpeg");
+        final java.awt.Image image = defaultToolkit.getImage(imageResource);
+
+        // this is new since JDK 9
+        final Taskbar taskbar = Taskbar.getTaskbar();
+
+        try {
+            // set icon for mac os (and other systems which do support this method)
+            taskbar.setIconImage(image);
+        }
+        catch (final UnsupportedOperationException e) {
+            System.out.println("The os does not support: 'taskbar.setIconImage'");
+        }
+        catch (final SecurityException e) {
+            System.out.println("There was a security exception for: 'taskbar.setIconImage'");
+        }
     }
 
 }
--- src/main/java/org/projectjinxers/ui/editor/EditorView.java
+++ src/main/java/org/projectjinxers/ui/editor/EditorView.java
@@ -13,10 +13,36 @@
  */
 package org.projectjinxers.ui.editor;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.projectjinxers.ui.ProjectJinxers;
 import org.projectjinxers.ui.common.PJView;
 
 import com.dansoftware.mdeditor.MarkdownEditorControl;
+import com.dansoftware.mdeditor.MarkdownEditorControl.ViewMode;
+import com.dansoftware.mdeditor.MarkdownEditorSkin;
+import com.vladsch.flexmark.ext.attributes.AttributeNode;
+import com.vladsch.flexmark.ext.attributes.AttributesExtension;
+import com.vladsch.flexmark.ext.attributes.AttributesNode;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.ast.NodeVisitor;
+import com.vladsch.flexmark.util.ast.VisitHandler;
+import com.vladsch.flexmark.util.collection.iteration.ReversiblePeekingIterable;
+import com.vladsch.flexmark.util.sequence.BasedSequence;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
 
 /**
  * @author ProjectJinxers
@@ -31,26 +57,171 @@ public class EditorView implements PJView<EditorPresenter.EditorView, EditorPres
         return res;
     }
 
-    public MarkdownEditorControl editor;
+    @FXML
+    private MarkdownEditorControl editor;
 
     private EditorPresenter editorPresenter;
 
-    public MarkdownEditorControl getEditor() {
-        return editor;
-    }
-
-    public void setEditor(MarkdownEditorControl editor) {
-        this.editor = editor;
-    }
+    private boolean addChangeListener = true;
 
     @Override
     public void updateView() {
+        if (addChangeListener) {
+            editor.markdownProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                    if (newValue != null && newValue.trim().length() > 0) {
+                        Platform.runLater(() -> {
+                            applySkippedAttributes(newValue);
+                        });
+                    }
+                }
+            });
+            addChangeListener = false;
+        }
         editor.setMarkdown(editorPresenter.getMarkdown());
     }
 
     @Override
     public EditorPresenter getPresenter() {
         return editorPresenter;
+    }
+
+    private void applySkippedAttributes(String markdown) {
+        ViewMode viewMode = editor.getViewMode();
+        if (viewMode != ViewMode.EDITOR_ONLY) {
+            MarkdownEditorSkin skin = (MarkdownEditorSkin) editor.getSkin();
+            Parser parser = Parser.builder().extensions(Collections.singleton(AttributesExtension.create())).build();
+
+            Document node = parser.parse(markdown);
+
+            MDParser mdParser = new MDParser(node);
+            mdParser.visitor.visitChildren(node);
+            if (mdParser.imageAttributes != null) {
+                Parent vbox = (Parent) skin.getChildren().get(0);
+                // vbox > splitPane > previewArea
+                ObservableList<Node> children = vbox.getChildrenUnmodifiable();
+                for (Node child : children) {
+                    if (child instanceof SplitPane) {
+                        SplitPane splitPane = (SplitPane) child;
+                        ObservableList<Node> items = splitPane.getItems();
+                        ScrollPane scrollPane;
+                        if (viewMode == ViewMode.PREVIEW_ONLY) {
+                            scrollPane = (ScrollPane) items.get(0);
+                        }
+                        else {
+                            scrollPane = (ScrollPane) items.get(1);
+                        }
+                        Parent preview = (Parent) scrollPane.getContent();
+                        adjustViews(preview, mdParser.imageAttributes);
+                    }
+                }
+            }
+        }
+    }
+
+    private void adjustViews(Parent parent, Map<String, AttributesNode> imageAttributes) {
+        ObservableList<Node> childrenUnmodifiable = parent.getChildrenUnmodifiable();
+        for (Node child : childrenUnmodifiable) {
+            if (child instanceof ImageView) {
+                String url = ((ImageView) child).getImage().getUrl();
+                AttributesNode attributes = imageAttributes.get(url);
+                if (attributes != null) {
+                    adjustImageView((ImageView) child, attributes);
+                }
+            }
+            else if (child instanceof Parent) {
+                adjustViews((Parent) child, imageAttributes);
+            }
+        }
+    }
+
+    private void adjustImageView(ImageView imageView, AttributesNode attributes) {
+        double width = 0;
+        double height = 0;
+        ReversiblePeekingIterable<com.vladsch.flexmark.util.ast.Node> children = attributes.getChildren();
+        for (com.vladsch.flexmark.util.ast.Node childNode : children) {
+            AttributeNode attribute = (AttributeNode) childNode;
+            BasedSequence name = attribute.getName();
+            BasedSequence value = attribute.getValue();
+
+            boolean isWidth;
+            boolean isHeight;
+            if (name.equals("width")) {
+                isWidth = true;
+                isHeight = false;
+            }
+            else {
+                isWidth = false;
+                if (name.equals("height")) {
+                    isHeight = true;
+                }
+                else {
+                    isHeight = false;
+                }
+            }
+            if (isWidth || isHeight) {
+                String val = value.toString();
+                boolean isRelative;
+                if (val.endsWith("px")) {
+                    val = val.replace("px", "");
+                    isRelative = false;
+                }
+                else if (val.endsWith("%")) {
+                    val = val.replace("%", "");
+                    isRelative = true;
+                }
+                else {
+                    isRelative = false;
+                }
+                try {
+                    double doubleVal = Double.parseDouble(val);
+                    if (isWidth) {
+                        width = doubleVal;
+                        if (isRelative) {
+                            width *= imageView.getParent().getParent().getBoundsInParent().getWidth();
+                        }
+                    }
+                    else {
+                        height = doubleVal;
+                        if (isRelative) {
+                            height *= imageView.getParent().getParent().getBoundsInParent().getHeight();
+                        }
+                    }
+                }
+                catch (NumberFormatException e) {
+
+                }
+            }
+
+        }
+
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(height);
+        imageView.setPreserveRatio(true);
+    }
+
+    static class MDParser {
+
+        private NodeVisitor visitor = new NodeVisitor(
+                new VisitHandler<>(com.vladsch.flexmark.ast.Image.class, this::visit));
+
+        private Map<String, AttributesNode> imageAttributes;
+
+        MDParser(Document document) {
+
+        }
+
+        private void visit(com.vladsch.flexmark.ast.Image image) {
+            com.vladsch.flexmark.util.ast.Node next = image.getNext();
+            if (next instanceof AttributesNode) {
+                if (imageAttributes == null) {
+                    imageAttributes = new HashMap<>();
+                }
+                imageAttributes.put(image.getUrl().toString(), (AttributesNode) next);
+            }
+        }
+
     }
 
 }
